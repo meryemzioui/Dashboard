adf18a51441f7a59e205a5133f8e1c45e49a57e5
meryem zioui <meryem.zioui@gmail.com>
2024-04-16 13:05:05 +0100

    set up Colors and MUI Light and Dark Mode Themes with Redux Toolkit

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,12 +1,22 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { ThemeSetting } from "./Theme";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
 
-function App() {
-  
+const App = () => {
+  const mode = useSelector((state) => {
+    state.global.mode;
+  });
+  const Theme = useMemo(() => createTheme(ThemeSetting(mode)), [mode]);
 
   return (
-    <>
-      hello
-    </>
-  )
-}
+    <div className="app">
+      <ThemeProvider Theme={Theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
+};
 
-export default App
+export default App;
